2e7ebf85dacffef2d07d4e3ec7a3e6b5a400cb16
jeiberth <jeiberth_camilo@hotmail.com>
2025-04-15 19:55:53 -0400

    Updates

--- assets/billPaymentApi-YImevZ8C.js
+++
@@ -1,1 +0,0 @@
-import"./axios-DoXh8n1i.js";class i{getBillsUnpaid(){return new Promise(e=>{e()})}payBills(){return new Promise(e=>{e()})}denyTenantApplication(e){return new Promise(n=>{n()})}getPayments(){return new Promise(e=>{e()})}}export{i as t};
